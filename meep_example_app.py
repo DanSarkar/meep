a654da4ff94ce4258529a958bb5d3dcc55c01083
https://github.com/ctb/cse891-fall-2011-test.git <Dan@Dan-PC.(none)>
2012-01-24 15:23:50 -0500

    Repy function

--- meep_example_app.py
+++ meep_example_app.py
@@ -2,6 +2,8 @@ import meeplib
 import traceback
 import cgi
 
+#Updated for HW1 8:15 Jan16
+
 def initialize():
     # create a default user
     u = meeplib.User('test', 'foo')
@@ -57,13 +59,12 @@ WSGI app object.
 
         s = []
         for m in messages:
-            s.append('id: %d<p>' % (m.id))
+            s.append('id: %d<p>' % (m.id,))
             s.append('title: %s<p>' % (m.title))
             s.append('message: %s<p>' % (m.post))
             s.append('author: %s<p>' % (m.author.username))
-            s.append('<form action="delete_message" method="POST"><input type="hidden" name="id" value="%d"><input type="submit" value="Delete Message"></form>' % (m.id))
+            s.append('<form action="delete_action" method="POST"><input type="hidden" name="messageID" value="%d"><input type="submit" value="Delete Message"></form>' % (m.id))
             s.append('<hr>')
-
         s.append("<a href='../../'>index</a>")
             
         headers = [('Content-type', 'text/html')]
@@ -95,22 +96,30 @@ WSGI app object.
         start_response("302 Found", headers)
         return ["message added"]
 
-    def delet_message(self, environ, start_response):
+    def reply_to_mwssage(self, environ, start_response):
+        print environ['wsgi.input']
+        form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
+        parentID = form['id'].value;
+        s = []
+        start_response("200 OK", [('Content-type', 'text/html')])
+        s.append("<p><p><a href = '../../'>Return to Index</a>")
+        return "".join(s)
+        
+    def delete_action(self, environ, start_response):
         print environ['wsgi.input']
         form = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
 
-        # Get the message id from form, convert to int
-        id = form['id'].value
-        message_id = int(id)
-
-        # Get message using built in function, delete the message
-        message = meeplib.get_message(message_id)
+        id = form['messageID'].value
+        message = meeplib.get_message(int(id))
         meeplib.delete_message(message)
-
+                     
         headers = [('Content-type', 'text/html')]
         headers.append(('Location', '/m/list'))
         start_response("302 Found", headers)
         return ["message deleted"]
+        
+
+        
     
     def __call__(self, environ, start_response):
         # store url/function matches in call_dict
@@ -120,7 +129,7 @@ WSGI app object.
                       '/m/list': self.list_messages,
                       '/m/add': self.add_message,
                       '/m/add_action': self.add_message_action,
-                      '/m/delete_message': self.delete_message
+                      '/m/delete_action': self.delete_action
                       }
 
         # see if the URL is in 'call_dict'; if it is, call that function.
@@ -140,3 +149,29 @@ WSGI app object.
             status = '500 Internal Server Error'
             start_response(status, [('Content-type', 'text/html')])
             return [x]
+
+        def print_messages(m, s, level):
+            if(level != 0):
+                s.append('<blockquote>')
+                s.append('<hr>')
+                s.append('id: %d<br/>' % (m.id,))
+                s.append('title: %s<br/>' % (m.title))
+                s.append('message: %s<br/>' % (m.post))
+                s.append('author: %s<p>' % (m.author.username))
+                s.append("""
+                <form action="add" method="POST">
+                <input type = 'submit' value = 'Reply'>
+                <input type = 'hidden' name ='pID' value = %d>
+                </form>""" % (m.id))
+                s.append("""
+                <form action = 'delete' method = 'POST'>
+                <input type = 'submit' value = 'Delete Post'>
+                <input type = 'hidden' name = 'id' value = '%d'>
+                </form>""" % (m.id))
+
+                if(m.replies != []):
+                    for r in m.replies:
+                        print_messages(meeplib.get_message(r), s, level + 1)
+                        s.append('</blockquote>')
+            
+                return s
